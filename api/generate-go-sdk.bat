bab64d3d7328a430b464dc7ee9ad15d1619a37e3
LaysDragon <laysdra7265@gmail.com>
2020-09-22 17:53:23 +0800

    remove submodule feature of api base package

--- api/generate-go-sdk.bat
+++ api/generate-go-sdk.bat
@@ -2,7 +2,7 @@ rmdir /Q /S ..\base
 call openapi-generator generate -i openapi.yaml -g go-experimental -t template/go-experimental -c config/go-experimental.yaml --type-mappings=ecpay-date-time=ECPayDateTime  --type-mappings=integer=int -o ../base
 rem call openapi-generator generate -i openapi.yaml -g go -t template/go-experimental -c config/go-experimental.yaml --type-mappings=ecpay-date-time=ECPayDateTime -o ../base
 cd ../base
-rem del go.mod
+del go.mod
 cd ..
 go fmt
 cd api
--- go.mod
+++ go.mod
@@ -3,10 +3,8 @@ module github.com/Laysi/go-ecpay-sdk
 go 1.14
 
 require (
-	github.com/Laysi/go-ecpay-sdk/base v0.0.0
 	github.com/fatih/structs v1.1.0
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.2
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
 )
-
-replace github.com/Laysi/go-ecpay-sdk/base => ./base
